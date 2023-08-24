0bdf71975ffb4d9edff3112bb6f97efae591a5fd
young1the <youngthedev@gmail.com>
2023-08-24 16:48:08 +0900

    build: spring initializr

---
+++ src/main/java/kr/co/chunjae/gochowoo/GochowooApplication.java
@@ -0,0 +1,13 @@
+package kr.co.chunjae.gochowoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GochowooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GochowooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/chunjae/gochowoo/GochowooApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.chunjae.gochowoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GochowooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

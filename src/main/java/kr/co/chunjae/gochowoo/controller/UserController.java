1e8af5b573cccfbb830a5bb5bc405a0cadfd4ea5
young1the <youngthedev@gmail.com>
2023-08-25 13:47:02 +0900

    유저기능 구현중

---
+++ src/main/java/kr/co/chunjae/gochowoo/controller/UserController.java
@@ -0,0 +1,21 @@
+package kr.co.chunjae.gochowoo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("")
+    public String showUserPage() { return "views/user/index"; }
+    @GetMapping("/login")
+    public String showLoginPage() { return "views/user/login"; }
+
+    @GetMapping("/join")
+    public String showJoinPage() { return "views/user/join"; }
+
+}

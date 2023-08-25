d696560849c02c4bf304690db899b938c3e80196
jangheeda <woojang95@gmail.com>
2023-08-25 14:03:53 +0900

    add: Entity
    Co-authored-by: young1the <youngthedev@gmail.com>

---
+++ src/main/java/kr/co/chunjae/gochowoo/model/Cart.java
@@ -0,0 +1,26 @@
+package kr.co.chunjae.gochowoo.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "t_cart")
+@Getter@Setter
+public class Cart {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @ManyToOne (cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne (cascade = CascadeType.ALL)
+    @JoinColumn(name = "pokemon_id")
+    private Pokemon pokemon;
+    @Column
+    private int amount = 1;
+
+}

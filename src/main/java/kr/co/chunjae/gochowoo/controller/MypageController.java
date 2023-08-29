8c916eb860e2034ad89fa901b8bb58bf9f07ccd4
SuperRookiee <hyunuk0417@gmail.com>
2023-08-29 18:50:23 +0900

    fix: mypage 유저 데이터

--- src/main/java/kr/co/chunjae/gochowoo/controller/MypageController.java
+++ src/main/java/kr/co/chunjae/gochowoo/controller/MypageController.java
@@ -1,14 +1,18 @@
 package kr.co.chunjae.gochowoo.controller;
 
+import kr.co.chunjae.gochowoo.model.User;
 import kr.co.chunjae.gochowoo.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @AllArgsConstructor
@@ -18,7 +22,9 @@ public class MypageController {
     final UserService userService;
 
     @GetMapping()
-    public String showShopPage() {
+    public String showShopPage(@SessionAttribute(name = "email") String email, Model model) {
+        User user = userService.userInfo(email);
+        model.addAttribute("user", user);
         return "views/mypage/mypage";
     }
 
